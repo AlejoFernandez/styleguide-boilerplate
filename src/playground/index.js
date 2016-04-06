c7b2a7e10540464429c22a0eaef26ccbe39c5cfd
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-04-06 19:25:21 -0300

    Add WebFooter component. Close #6

--- src/playground/index.js
+++ src/playground/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Playground from 'component-playground';
-import Search from '../components/search';
-import searchExample from 'raw!./examples/search.example';
-import Header from '../components/header';
-import headerExample from 'raw!./examples/header.example';
+import { Search, Header, WebFooter } from '../components';
+import { searchExample, headerExample, WebFooterExample } from './examples';
 
 export default function Index() {
   return (
     <div className="component-documentation">
       <Playground codeText={searchExample} scope={{ React, Search }} />
       <Playground codeText={headerExample} scope={{ React, Header }} />
+      <Playground codeText={WebFooterExample} scope={{ React, WebFooter }} />
     </div>
   );
 }
