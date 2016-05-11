93873ea646a22cbb0cbc436eeba7b6aac762abfd
Ricky Rauch <rickyrauch@gmail.com>
2016-05-11 17:38:34 -0300

    fixes

--- landing/index.js
+++ landing/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Playground from 'component-playground';
-import { Modal } from '../src';
+import { Search, WebHeader, WebFooter, Modal } from '../src';
 
 export default function Index() {
   return (
     <div className="component-documentation">
-      <Playground codeText="ssds" scope={{ React, Modal }} />
+      
     </div>
   );
 }
